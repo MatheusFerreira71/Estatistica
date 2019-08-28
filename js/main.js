67ad948fee05595f7841fecbf2af01ae204e188a
marioroncca@hotmail.com <marioroncca@hotmail.com>
2019-08-27 22:09:26 -0300

    Começo do JS.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// obj = {
+//     nome: "Idade",
+//     tipoAnalise: "População",
+//     separatriz: 0.1,
+//     tipoVariavel: "QC",
+//     dados: [12, 23, 23, 12, 23, 21]
+// }
+let tabelas = [];
+function adicionarVariavel(){
+    let nome = document.getElementById('nomeVariavel').value;
+    console.log(nome);
+}
