bbddc26163defaade82c5c9bf6763eb4bc12e3e5
Matheus Ferreira <matheusfernandofreire@hotmail.com>
2019-09-09 16:31:21 -0300

    Gráfico nominal 90%.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,225 @@
 let tabelas = [];
 let barra = document.getElementById('RangeSeparatriz');
 
+Highcharts.setOptions({
+    colors: Highcharts.map(Highcharts.getOptions().colors, function (color) {
+        return {
+            radialGradient: {
+                cx: 0.5,
+                cy: 0.3,
+                r: 0.7
+            },
+            stops: [
+                [0, color],
+                [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+            ]
+        };
+    })
+});
+
+Highcharts.theme = {
+    colors: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066',
+        '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+    chart: {
+        backgroundColor: {
+            linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+            stops: [
+                [0, '#2a2a2b'],
+                [1, '#3e3e40']
+            ]
+        },
+        style: {
+            fontFamily: '\'Unica One\', sans-serif'
+        },
+        plotBorderColor: '#606063'
+    },
+    title: {
+        style: {
+            color: '#E0E0E3',
+            textTransform: 'uppercase',
+            fontSize: '20px'
+        }
+    },
+    subtitle: {
+        style: {
+            color: '#E0E0E3',
+            textTransform: 'uppercase'
+        }
+    },
+    xAxis: {
+        gridLineColor: '#707073',
+        labels: {
+            style: {
+                color: '#E0E0E3'
+            }
+        },
+        lineColor: '#707073',
+        minorGridLineColor: '#505053',
+        tickColor: '#707073',
+        title: {
+            style: {
+                color: '#A0A0A3'
+
+            }
+        }
+    },
+    yAxis: {
+        gridLineColor: '#707073',
+        labels: {
+            style: {
+                color: '#E0E0E3'
+            }
+        },
+        lineColor: '#707073',
+        minorGridLineColor: '#505053',
+        tickColor: '#707073',
+        tickWidth: 1,
+        title: {
+            style: {
+                color: '#A0A0A3'
+            }
+        }
+    },
+    tooltip: {
+        backgroundColor: 'rgba(0, 0, 0, 0.85)',
+        style: {
+            color: '#F0F0F0'
+        }
+    },
+    plotOptions: {
+        series: {
+            dataLabels: {
+                color: '#F0F0F3',
+                style: {
+                    fontSize: '13px'
+                }
+            },
+            marker: {
+                lineColor: '#333'
+            }
+        },
+        boxplot: {
+            fillColor: '#505053'
+        },
+        candlestick: {
+            lineColor: 'white'
+        },
+        errorbar: {
+            color: 'white'
+        }
+    },
+    legend: {
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        itemStyle: {
+            color: '#E0E0E3'
+        },
+        itemHoverStyle: {
+            color: '#FFF'
+        },
+        itemHiddenStyle: {
+            color: '#606063'
+        },
+        title: {
+            style: {
+                color: '#C0C0C0'
+            }
+        }
+    },
+    credits: {
+        style: {
+            color: '#666'
+        }
+    },
+    labels: {
+        style: {
+            color: '#707073'
+        }
+    },
+
+    drilldown: {
+        activeAxisLabelStyle: {
+            color: '#F0F0F3'
+        },
+        activeDataLabelStyle: {
+            color: '#F0F0F3'
+        }
+    },
+
+    navigation: {
+        buttonOptions: {
+            symbolStroke: '#DDDDDD',
+            theme: {
+                fill: '#505053'
+            }
+        }
+    },
+
+    // scroll charts
+    rangeSelector: {
+        buttonTheme: {
+            fill: '#505053',
+            stroke: '#000000',
+            style: {
+                color: '#CCC'
+            },
+            states: {
+                hover: {
+                    fill: '#707073',
+                    stroke: '#000000',
+                    style: {
+                        color: 'white'
+                    }
+                },
+                select: {
+                    fill: '#000003',
+                    stroke: '#000000',
+                    style: {
+                        color: 'white'
+                    }
+                }
+            }
+        },
+        inputBoxBorderColor: '#505053',
+        inputStyle: {
+            backgroundColor: '#333',
+            color: 'silver'
+        },
+        labelStyle: {
+            color: 'silver'
+        }
+    },
+
+    navigator: {
+        handles: {
+            backgroundColor: '#666',
+            borderColor: '#AAA'
+        },
+        outlineColor: '#CCC',
+        maskFill: 'rgba(255,255,255,0.1)',
+        series: {
+            color: '#7798BF',
+            lineColor: '#A6C7ED'
+        },
+        xAxis: {
+            gridLineColor: '#505053'
+        }
+    },
+
+    scrollbar: {
+        barBackgroundColor: '#808083',
+        barBorderColor: '#808083',
+        buttonArrowColor: '#CCC',
+        buttonBackgroundColor: '#606063',
+        buttonBorderColor: '#606063',
+        rifleColor: '#FFF',
+        trackBackgroundColor: '#404043',
+        trackBorderColor: '#404043'
+    }
+};
+
+// Apply the theme
+Highcharts.setOptions(Highcharts.theme);
+
 function separador(vetor) {
     let Quantidades = {};
     let aux;
@@ -216,8 +435,8 @@ function previewDados(vetorTabelas) {
 }
 
 function resetPreview() {
-    let cabecalho = document.getElementById('linhaCabecalhoPreview').innerHTML = '';
-    let corpo = document.getElementById('corpoTabelaPreview').innerHTML = '';
+    document.getElementById('linhaCabecalhoPreview').innerHTML = '';
+    document.getElementById('corpoTabelaPreview').innerHTML = '';
 }
 
 function mudarSeparatriz() {
@@ -308,7 +527,7 @@ function calcular(vetorTabelas) {
                                     <th scope="col">FAC%</th>`;
             let FrequenciaAtual = 0, FrequenciaPorAtual = 0;
             //Escreve a Tabela
-            let js = 1, objMediana = {}
+            let js = 1, objMediana = {}, vetorGrafico = [];
             for (let z in obejeto) {
                 let linhaAtual = linhas[js];
                 linhaAtual.innerHTML = `<td>${z}</td>
@@ -317,13 +536,17 @@ function calcular(vetorTabelas) {
                                             <td>${FrequenciaAtual += obejeto[z]}</td>
                                             <td>${(FrequenciaPorAtual += obejeto[z] / totalFrequencia * 100).toFixed(2)}</td>`;
                 objMediana[z] = FrequenciaAtual;
+                let objGrafico = {};
+                objGrafico.name = z;
+                objGrafico.y = parseFloat((obejeto[z] / totalFrequencia * 100).toFixed(2));
+                vetorGrafico.push(objGrafico);
                 js++
             }
             grupoResults.innerHTML += `
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-md-2"></div>
-                        <div class="col-md-8">
+                    <div class="col-md-1"></div>
+                        <div class="col-md-10">
                             <div class="card my-3 border-info" style="width: auto;">
                                 <div class="card-header bg-dark text-white text-center" style="font-weight: bold;">Resultados Estatísticos</div>
                                 <ul class="list-group list-group-flush">
@@ -332,10 +555,56 @@ function calcular(vetorTabelas) {
                                 </ul>
                             </div>
                         </div>
-                <div class="col-md-2"></div>    
+                <div class="col-md-1"></div>    
+                </div>
+            </div>
+            <div class="container-fluid my-3">
+                <div class="row">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-10">
+                        <div id="chart${vetorTabelas[i].nome}" style="height: 400px; width: auto;"></div>
+                    </div>
+                    <div class="col-md-1"></div>
                 </div>
             </div>
             `;
+
+            //Gráfico
+            Highcharts.chart(`chart${vetorTabelas[i].nome}`, {
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false,
+                    type: 'pie',
+                    options3d: {
+                        enabled: true,
+                        alpha: 45,
+                        beta: 0
+                    }
+                },
+                title: {
+                    text: `${vetorTabelas[i].nome}`
+                },
+                tooltip: {
+                    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>',
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: false
+                        },
+                        showInLegend: true,
+                        depth: 35
+                    }
+                },
+                series: [{
+                    name: 'FR%',
+                    colorByPoint: true,
+                    data: vetorGrafico
+                }]
+            });
 
         } else if (vetorTabelas[i].tipoVar == "Quantitativa Discreta") {
             vetorTabelas[i].dados.sort();
